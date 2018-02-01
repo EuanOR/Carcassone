0bf288517bb5ffad47aaf2ad84d13a81a6a78d83
Henry-Tan <35701411+Henry-Tan@users.noreply.github.com>
2018-02-01 14:48:11 +0000

    Update gridV2.py

--- gridV2.py
+++ gridV2.py
@@ -1,4 +1,5 @@
-from TileV1 import *
+from Tile import *
+from TileTypes import *
 
 """
     A new version of Grid, instead of using 2D array we will now use a sparse array. [ [ [x1, y1], TileObject], [ [x2, y2], TileObject] ]
@@ -7,9 +8,9 @@ from TileV1 import *
     that will look like this -> [ [ [0, 0], InitialTileObject ] ]. 
     If we place another tile ontop of the initial tile we will have [ [ [0, 0], initialTileObject] , [ [0, -1], Tile2Object] ]
 
-    We have a checkAvailbility function which will return a list with all the possible valid locations on the grid. 
+    We have a checkAvailbility function which will return a list with all the open locations on the grid. 
     This function does not valid the tile placement yet, for example checking if the road on Tile1 matches the road on Tile2.
-    [[0, -1], [1, 0], [0, 1], [-1, 0]]  is an example of the possible valid locations of the initial tile. Each location is held in a list with x coord and y coord
+    [[0, -1], [1, 0], [0, 1], [-1, 0]]  is an example of the open locations of the initial tile. Each location is held in a list with x coord and y coord
 
 """
 class Grid:
@@ -20,8 +21,8 @@ class Grid:
         self._center = 0
         self._initialTile = tile
 
-        # A list where it holds all the possible valid locations.
-        self._possibleValidLocation = []
+        # A list where it holds all the open locations.
+        self._openLocation= []
         self.createGrid()
 
     def __str__(self):
@@ -34,18 +35,16 @@ class Grid:
 
     # Places tile at grid location x, y
     def insertTile(self, x, y, tile):
-        tile._x = x
-        tile._y = y
 
         self.checkAvailability()
         self._grid += [[[x,y], tile]]
         self.checkAvailability()
 
-        # After placing the tile remove it from possible valid location array
-        if [x,y] in self._possibleValidLocation:
-            self._possibleValidLocation.remove([x,y])
+        # After placing the tile remove it from open location array
+        if [x,y] in self._openLocation:
+            self._openLocation.remove([x,y])
 
-    # Returns a list of possible valid locations on the grid
+    # Returns a list of open location valid locations on the grid
     def checkAvailability(self):
 
         tempResultArray = []
@@ -62,24 +61,21 @@ class Grid:
             left = [placedTileX - 1, placedTileY]
 
             # Checking if it is free tile, if it is free tile then add it to resultArray.
-            if top != placedTile and top not in self._possibleValidLocation and top not in tempResultArray and top != [0,0]:
+            if top != placedTile and top not in self._openLocation and top not in tempResultArray and top != [0,0]:
                 tempResultArray += [top]
-            if right != placedTile and right not in self._possibleValidLocation and right not in tempResultArray and right != [0,0]:
+            if right != placedTile and right not in self._openLocation and right not in tempResultArray and right != [0,0]:
                 tempResultArray += [right]
-            if bottom != placedTile and bottom not in self._possibleValidLocation and bottom not in tempResultArray and bottom != [0,0]:
+            if bottom != placedTile and bottom not in self._openLocation and bottom not in tempResultArray and bottom != [0,0]:
                 tempResultArray += [bottom]
-            if left != placedTile and left not in self._possibleValidLocation and left not in tempResultArray and left != [0,0]:
+            if left != placedTile and left not in self._openLocation and left not in tempResultArray and left != [0,0]:
                 tempResultArray += [left]
 
-        self._possibleValidLocation = tempResultArray
-        return(self._possibleValidLocation)
+        self._openLocation = tempResultArray
+        return(self._openLocation)
 
 def main():
-    tile1 = Tile()
-    grid = Grid(tile1)
+    initialTile = IntialTile()
+    grid = Grid(initialTile)
 
-    tile2 = Tile()
-    grid.insertTile(0, -1, tile2) # Adding a tile ontop of the center initial tile
-
-    print(grid._possibleValidLocation)
+    print(grid._openLocation)
 main()
