c3671ec4e3147fce3f2704e542364bce3ce55032
StephenOR <35529975+StephenOR@users.noreply.github.com>
2018-02-01 15:26:30 +0000

    Update Meeple.py

--- Meeple.py
+++ Meeple.py
@@ -1,22 +1,27 @@
 class Meeple(object):
-
+ """Creates a meeple Object
+    01/02/18 - Stephen and Euan
+ """
+    
+  
+    """Initiates a Meeple Object.
+       player points to the player object which "owns" the Meeple
+       placed is a boolean which represents if the Meeple is placed on the board or not
+       colour is a string which represents the Meeple's colour, this is found using the Player's getColour method"""
     def __init__(self,player):
         self._player =  player
         self._placed = False
         self._colour = self._player.getColour()
-
+    
+    #Creates a string representation for a Meeple
     def __str__(self):
         outstr = ""
-        outstr += ("Meeple Colour :%s\n Meeple Player :%s\nMeeple Placed :%b",(self._colour,self._player._name,self._placed))
+        outstr += ("Meeple Colour :%s\nMeeple Player :%s\nMeeple Placed :%r"%(self._colour,self._player._name,self._placed))
         return outstr
 
     #Returns the colour of the meeple
     def getColour(self):
         return self._colour
-
-    #Associates the meeple with a player
-    def setPlayer(self,player):
-        self._player = player
 
     #Retunrs the player using the meeple.
     def getPlayer(self):
@@ -25,6 +30,25 @@ class Meeple(object):
     #If a meeple is placed on a tile this sets placed to True
     def place(self):
         self._placed = True
-
+        
+    #If a meeple is removed from a tile this sets placed to False
     def take_back(self):
         self._placed = False
+
+"""Meeple needs to be tested within the Player class, as a Meeple cannot be created without a player object
+   The following is the Meeples test suite.
+   
+   if __name__=="__main__":
+    Player1=Player("Brian")
+    Player1.setColour("Green")
+    Meeple1=Meeple(Player1)
+    colourTest=Meeple1.getColour()
+    print(colourTest)
+    playerTest=Meeple1.getPlayer()
+    print(playerTest)
+    Meeple1.place()
+    print(Meeple1)
+    Meeple1.take_back()
+    print(Meeple1)
+    
+ """
