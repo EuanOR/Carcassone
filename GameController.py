b4a9fe03b8482cf48b37bde07cf5678daf2f93ef
EwyOR <35605294+EwyOR@users.noreply.github.com>
2018-02-05 11:30:21 +0000

    Add files via upload

--- GameController.py
+++ GameController.py
@@ -12,6 +12,19 @@ class GameController:
 	def __init__(self):
 		#Note: Only added in the functionality I needed for the isLandmarkComplete
 		self._grid = Grid(InitialTile())
+        self._players = []
+        self._playing = player
+        self._gameOver = False
+    
+    #Just the bones of how the game could go through each turn
+    #Each player has a turn and places a tile and a meeple until the game is over
+    #def play(self):
+        #while not self._gameOver:
+            #for p in self._players:
+                #self._playing = player
+
+    def joinGame(self,player):
+        self._players.append(player)
 	
 	def isLandmarkComplete(self, landmark):
 		"""Returns True if the landmark 'landmark' is complete."""
@@ -50,6 +63,23 @@ class GameController:
     
     def placeTile(self,tile,x,y):
         self._grid.insertTile(x,y,tile)
+        self.placeMeeple(tile)
     
-    def placeMeeple(self):
-        pass
+    def placeMeeple(self,player = self._playing, tile):
+        meeple = player.placeMeeple()
+        landmarks = []
+        
+        if tile._top != "Grass":
+            landmarks.append(tile._top)
+        
+        if tile._bottom != "Grass":
+            landmarks.append(tile._bottom)
+        
+        if tile._left != "Grass":
+            landmarks.append(tile._left)
+        
+        if tile._right != "Grass":
+            landmarks.append(tile._right)
+        
+        for l in landmarks:
+            print(l)
