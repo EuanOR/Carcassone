519ee8121380326bdb369942d045c976519f7a3f
Catherine Bowen <catherinebowen96@gmail.com>
2018-02-07 12:35:31 +0000

    Added in placeTile

--- GameController.py
+++ GameController.py
@@ -82,8 +82,103 @@ class GameController:
                     return False
 	return True
     
+    def getNeighbour(self, tile, direction):
+        """Returns a neighbour of tile 'tile'
+        direction is a string to specify which neighbour 
+        eg "left" for the neighbouring tile on the left"""
+        if direction == "left":
+            return self._grid.getTile(tile._xPos-1, tile._yPos)
+        if direction == "right":
+            return self._grid.getTile(tile._xPos+1, tile._yPos)
+        if direction == "top":
+            return self._grid.getTile(tile._xPos, tile._yPos-1)
+        if direction == "bottom":
+            return self._grid.getTile(tile._xPos, tile._yPos+1)
+
+    def finishLandmark(self, landmark):
+        """Frees up meeples and assigns scores to players."""
+        owners = {}
+        for tile in landmark.getTiles():
+            if tile._meeple_placement == landmark:
+                #record meeple count for owner
+                owner = tile._meeple.getPlayer()
+                owners[owner] = owners.get(owner, 0) + 1
+
+                #clear meeples from tile + player
+                owner._activeMeeples.remove(tile._meeple)
+                owner._inactiveMeeples.append(tile._meeple)
+                tile._meeple._placed = False
+
+                tile._meeple = None
+                tile._meeple_placement = None
+
+                #should I remove the meeple from landmark??
+                #i dont think not removing it makes any odds 
+                #because when its finished its not like you can shove anything else on?
+
+        #determine player(s) had the most meeples on the landmark
+        winners = []
+        max_meeples = 0
+        for p in owners:
+            if owners[p] > max_meeples:
+                max_meeples = owners[p]
+                winners = [p]
+            elif owners[p] == max_meeples:
+                winners.append(p)
+
+        #give them the points for the landmark
+        for winner in winners:
+            winner._score += landmark.getScore()
+
+    def join(self, landmark1, landmark2):
+        """Joins two landmarks."""
+        if isinstance(landmark1, City):
+            if not isinstance(landmark2, City):
+                return
+            City.join(landmark1, landmark2)
+        elif isinstance(landmark1, Road):
+            if not isinstance(landmark2, Road):
+                return
+            Road.join(landmark1, landmark2)
+
+    def getTileSide(self, tile, side):
+        #just to reduce duplicate code with getting sides!
+        #might move to tile class later on? it seems a little messy here???
+        if side == "left":
+            return tile._left
+        elif side == "right":
+            return tile._right
+        elif side == "top":
+            return tile._top
+        elif side == "bottom":
+            return tile._bottom
+
     def placeTile(self,tile,x,y):
-    	self._grid.insertTile(x,y,tile)
+        """Place a tile on the grid."""
+        #list to make iterating with getTileSide easier
+        sides = ["left", "right", "top", "bottom"]
+        #dict to make getting the opposide side easier
+        opposite = {"left": "right", "right":"left", "top":"bottom", "bottom":"top" }
+        
+        if not self._grid.insertTile(x,y,tile):
+            raise InvalidTilePlacementException("insertTile failed")
+
+        for side in sides:
+            if not isinstance(self.getTileSide(tile, side), Grass):
+                neighbour = self.getNeighbour(tile, side)
+                if neighbour != None:
+                    #join the side of the tile to the touching side of the neighbour
+                    self.join(self.getTileSide(tile, side), self.getTileSide(neighbour, opposite[side]))
+                    if self.isLandmarkComplete(self.getTileSide(tile, side)):
+                        #this gets score, frees up meeples etc
+                        self.finishLandmark(self.getTileSide(tile, side))
+        # returns a list of valid places to place a meeple (to be used with placeMeeple()
+	validMeeplePlacements = []
+        for side in sides:
+            landmark = self.getTileSide(tile, side)
+            if (not self.isLandmarkComplete(landmark)) and landmark._meeples == []:
+                validMeeplePlacements.append(landmark)
+        return validMeeplePlacements
 
     def placeMeeple(self,tile,user_choice):
         #This function places a Meeple on a landmark
@@ -99,3 +194,21 @@ class GameController:
                 print("No available Meeples")
         else:
             print("Cannot place on grass")
+
+def main():
+    end1 = FourWayCrossroad()
+    end2 = FourWayCrossroad()
+    gc = GameController()
+    Brian = Player("Brian")
+    Brian.createMeeples()
+    gc.joinGame(Brian)
+
+    meeple_placements = gc.placeTile(end1, -1, 0)
+    
+    gc.placeMeeple(end1, end1._right)
+    gc.placeTile(end2, 1, 0)
+    
+    print("Brian's score=", Brian._score)
+
+if __name__ == "__main__":
+    main()
