d021979b0fa262e3bd632b0c90ec39fbe72012de
Eimear Foley <eimearfoley96@gmail.com>
2018-02-12 23:50:02 +0000

    Updated to include AJAX side code
    Addition of several functions and modifications to existing functions
    Comments included to all
    //TODO: ... is work yet to be done

--- grid.js
+++ grid.js
@@ -1,161 +1,263 @@
-    (function() {
+(function() {
 
-
-    /* I have set some protocols for the table.
-    *  Every cell in the table is have an co-ordinate id attribute. -> "0,1" "1,-1"
-    *  The valid tile image (FreeTile.png) tag has a class name of "unplaced".
-    *  Valid tiles (FreeTile.png) image are in every cell once the grid is created but it is set to hidden.
-    *  If a table cell is taken, the tile image tag will be set to "placed"
-    *  The use of IDs and Class names are so that you can retrieve all the elements by class name or id easier.
-    *  For example if you want the the cell at location -1, 2, call document.getElementbyId(id)
-    */
-
+    // Gameboard & sidebar container
     var div;
-    var validPlacesArray = [];
-
-    var nextTile;
+    // Relative path of image src
+    var currentTile;
+    // Current rotation of currentTile
+    var rotation = 0;
+    // Player index e.g. 0, 1, 2, 3
+    var player;
 
     document.addEventListener("DOMContentLoaded", init, false);
 
-    function init() {
+    function init(){
         div = document.querySelector("div");
-        createTable();
-        placeInitialTile(0,0);
-
-        showAvailableTiles(validPlacesArray);
-
-        // Required to get a tile off the tile deck display it. At the moment im just using a random tile and displaying it.
-        showDeckTile("./TileAssets/3T4R2G.png");
-        testAJAX();
+        startGame();
     }
 
-    function testAJAX() {
-        var url = "test.py?function=printString&paramname='hi'";
-        request = new XMLHttpRequest();
-        request.addEventListener("readystatechange", handle_response, false);
-        request.open("GET", url, true);
-        request.send(null);
+    //STARTS GAME
+    function startGame(){
+        //Create gameboard
+        createGameBoard();
+        //Place initial tile in grid cell [0,0]
+        placeStartTile();
+        //Get player name
+        getPlayer();
+        //Get tile for player's turn
+        getPlayerTile();
+        //Get valid places where player can place tile
+        showValidPlaces();
     }
 
-    function handle_response() {
-        if (request.readyState === 4) {
-            if (request.status === 200) {
-                if (request.responseText.trim() === "success") {
-                    listOfStrings = request.responseText.trim().split(" ");
-                    console.log(listOfStrings);
-                } 
-            }
-        }
-    }
-
-
-    // A function to create a 2D html table. Each cell has an co-ordinate ID attribute -> "0,0" , "-1,0"
-    function createTable() {
+    //CREATE TABLE FOR GAMEBOARD
+    function createGameBoard(){
+        // Create HTML table
         var table = document.createElement('table');
         table.cellPadding = "0";
         table.cellSpacing = "0";
         var tableBody = document.createElement('tbody');
 
-        var yStart = 1
-        for (var i=0; i < 3; i++) {
+        // y-coordinate for cell
+        var yStart = -1;
+
+        for (var i = 0; i < 3; i++){
+            // Create row
+            var row = document.createElement("tr");
+
+            // x-coordinate for cell
             var xStart = -1;
 
-            var row = document.createElement("tr");
-            for (var i2=0; i2 < 3; i2++) {
+            for (var j = 0; j < 3; j++){
+                // Create column
                 var cell = document.createElement("td");
+
+                // Adds an ID for each cell of table
                 cell.id = (((xStart).toString()).concat(",")).concat((yStart.toString()));
 
-                var freeTileImage = document.createElement('img');
-                freeTileImage.src = "./TileAssets/FreeTile.png";
-                freeTileImage.className = "unplaced";
-                freeTileImage.style.visibility = "hidden";
-                cell.appendChild(freeTileImage);
-                row.appendChild(cell);
+                // Add blank image for each cell
+                var emptyCell = document.createElement('img');
+                emptyCell.src = "TileAssets/FreeTile.png";
+                emptyCell.className = "unplaced";
+                emptyCell.style.visibility = "hidden";
+                cell.appendChild(emptyCell);
+
+                // Increment cell position
                 xStart ++;
             }
-            yStart--;
+            // Increment cell position
+            yStart ++;
             tableBody.appendChild(row);
         }
+        // Add table to container div and set a border
         table.appendChild(tableBody);
         div.appendChild(table);
         table.setAttribute("border", "1");
     }
 
+    // PLACE START TILE ON GAME BOARD
+    function placeStartTile(){
+        // Cell to place start tile
+        var cell = document.getElementById(0,0);
 
-    // A function to place the initial tile onto the table.
-    function placeInitialTile(x,y) {
-
-        // Construct a variable containing the string of x y co-ordinate -> param X and Y will turn into "X,Y". Eg X = 1, Y = 0 -> "1,0"
-        var stringLocation = (x.toString()).concat(",").concat((y.toString()));
-
-        var cell = document.getElementById(stringLocation);
-
-        // Creating a img element and append it to the center cell (initial tile)
         var image = document.createElement('img');
-        image.src = "./TileAssets/Start.PNG";
+        image.src = "TileAssets/Start.PNG";
         image.className = "placed";
         cell.innerHTML = "";
+
+        // Place tile in central grid cell [0,0]
         cell.appendChild(image);
-
-        // This only shows the available locations rather than valid places. Need ajax to retrieve valid location.
-        validPlacesArray = ["0,1","1,0", "0,-1", "-1,0"];
-
-        showAvailableTiles(validPlacesArray);
     }
 
-    // A function to show all the valid places on the table.
-    // If it is valid it will display a valid image on the table (FreeTile.png)
-    // Param validCoordinates should be a list that is retrieved by ajax of the valid coordinates.
-    function showAvailableTiles(validCoordinates) {
-        for (var i=0; i < validCoordinates.length; i++) {
+    // Get current Player name and meeples etc.
+    function getPlayer(){
+        var url = "getPlayer.py";
+        request = new XMLHttpRequest();
+        request.addEventListener("readystatechange", playerReceived, false);
+        request.open("GET", url, true);
+        request.send(null);
+    }
 
-            var cellImage = document.getElementById(validCoordinates[i]).firstChild;
+    // Handler for 'getPlayer()'
+    function playerReceived(){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                if (request.responseText.trim() != "problem") {
+                    //TODO: update display to show who's turn it is
+                    // request.responseText.trim() = player index in GameController players array
+                    // e.g. 0, 1, 2, 3
 
-            cellImage.style.visibility = "visible";
-            cellImage.className = "available";
+                    var player = document.getElementById(request.responseText.trim());
+                    player.className = "active";
 
-            cellImage.addEventListener("click", function() { insertTile(this.parentNode.id);});
+                    //TODO: Lock controls to this player
+                } 
+            }
+        }
+
+    }
+
+    //GET PLAYER THEIR TILE FOR THIS TURN
+    function getPlayerTile(){
+        var url = "getTile.py";
+        request = new XMLHttpRequest();
+        request.addEventListener("readystatechange", tileReceived, false);
+        request.open("GET", url, true);
+        request.send(null);    
+    }
+
+    function tileReceived(){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                if (request.responseText.trim() != "problem") {
+                    // request.responseText.trim() = "TileAssets/tile.png"
+                    // If successful update user display to new player tile
+                    var div = document.getElementById("deckTile");
+                    var image = document.createElement("img");
+                    image.src = request.responseText.trim();
+                    currentTile = request.responseText.trim();
+                    div.appendChild(image);
+                }
+            }
+        }
+
+    }
+
+    //SHOW AVAILABLE VALID CELLS
+    function showValidPlaces(rotation){
+        // Get array of available, valid cells
+        var url = "getValidPlaces.py?rotation=" + rotation;
+        request = new XMLHttpRequest();
+        request.addEventListener("readystatechange", validPlacesReceived, false);
+        request.open("GET", url, true);
+        request.send(null);
+    }
+
+    // Handles 'showValidPlaces()'
+    function validPlacesReceived(){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                if (request.responseText.trim() != "problem") {
+
+                    // If successful get list of valid cell locations
+                    var xYList = request.responseText.trim().split(" ");
+
+                    // Traverse through valid cell locations and update user display
+                    for (var i=0; i < xYList.length; i++) {
+
+                        var cellImage = document.getElementById(xYList[i]).firstChild;
+                        cellImage.style.visibility = "visible";
+                        // AVAILABLE VS VALID MUST ASK HENRY
+                        cellImage.className = "available";
+
+                        // Add event listeners to valid empty cells
+                        cellImage.addEventListener("click", function() { insertTile(this.parentNode.id);});
+                    }
+                }
+            }
         }
     }
 
-    // Insert a tile to the grid.
-    // Param id is a string of the cell's ID attribute that we want to place the tile to.
-    // NOT COMPLETED
-    function insertTile(id) {
-        var image = document.getElementById(id).childNodes;
-
-        image[0].src = nextTile;
-
-        image[0].className = "placed";
-
-        hideAvailableTiles();
-
-        //TODO: get a new tile from TileDeck + display it
-        //TODO: get the new available Tiles from python.
-        //TODO: Display the available tiles
-    }
-
-    // Function to hide all available tiles. 
-    // This function should be used when a tile is placed and we need to hide all the valid places for the next tile.
-    function hideAvailableTiles() {
+    function hideValidPlaces(){
+        // Array of cells marked 'available' 
         var unplacedCellImages = Array.from(document.getElementsByClassName("available"));
 
-        for (var i=0; i < unplacedCellImages.length; i++) {
+        // Traverse through array and hide each cell from user display
+        for (var i = 0; i < unplacedCellImages.length; i++) {
             unplacedCellImages[i].className = "unplaced";
             unplacedCellImages[i].setAttribute("style", "visibility:hidden");
-
-            unplacedCellImages[i].removeEventListener("click", function() {insertTile(this.parentNode.id);});
+            // Remove event listeners
+            unplacedCellImages[i].removeEventListener("click", function() {placeTile(this.parentNode.id);});
         }
     }
 
-    // A function to display the tile to the deckTile DIV.
-    function showDeckTile(tilePath) {
-        var div = document.getElementById("deckTile");
-
-        var image = document.createElement("img");
-        image.src = tilePath;
-        div.appendChild(image);
-        nextTile = tilePath;
+    //PLAYER PLACES TILE ON TURN
+    function placeTile(cellID){
+        // ASK HENRY WHAT EXACTLY THIS LINE DOES
+        var image = document.getElementById(id).childNodes;
+        image[0].src = currentTile;
+        image[0].className = "placed";
+        hideAvailableTiles();
+        //TODO: Update table to show new tile placement
+        //TODO: Send cell information to GameController
+        placeMeeple();
+        updateScore();
+        nextTurn();
     }
-}) 
-();
+
+    function placeMeeple(){
+        //TODO: ask GameController if meeple can be placed
+        //if meeple placement possible
+        //  ask user do you want to place meeple?
+        //  if True
+        //      place meeple
+        //      update display
+        //      send info back to game controller
+        //  else:
+        //      return
+    }
+
+    function nextTurn(){
+        rotation = 0;
+        getPlayer();
+        getPlayerTile();
+        showValidPlaces(rotation);
+    }
+
+    function rotateTile(){
+        //TODO: update user of tile rotation display
+        // Can do with CSS or JQUERY 
+        // ASK CATHY
+        if (rotation >= 3){
+            rotation = 0;
+        } else {
+            rotation ++;
+        }
+        showValidPlaces(rotation);
+    }
+
+    // Called in 'placeTile()'
+    function updateScore(){
+        //TODO: GET SCORE FROM GAME CONTROLLER
+        var url = "getScore.py" + rotation;
+        request = new XMLHttpRequest();
+        request.addEventListener("readystatechange", scoreReceived, false);
+        request.open("GET", url, true);
+        request.send(null);
+    }
+
+    // Handles 'updateScore()'
+    function scoreReceived(){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                if (request.responseText.trim() != "problem") {
+                    // scores = [21, 34, 12, 24]
+                    // scores[0] = Player 1's score
+                    var scores = request.responseText.trim().split(" ");
+                    //TODO: UPDATE LEADERBOARD
+                }
+            }
+        }
+    }
+
+});
