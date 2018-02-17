a4c4806eb6f783adef2b88ccc1db2bdc317c9230
Claire Foran <foranclaire@gmail.com>
2018-02-17 19:47:22 +0000

    Update getGameController.py

--- Website/getGameController.py
+++ Website/getGameController.py
@@ -9,9 +9,29 @@ def getGameController():
     if not 'playerID' in cookie:
         print("Error! Player ID not in cookie")
         exit()
-    sid = cookie['playerID'].value
+    playerID = cookie['playerID'].value
     player_session = open('player_sessions/sess_' + playerID, writeback=False)
     gameID = player_session['gameID']
+    player_session.close()
     game = open("game_sessions/sess_" + gameID, writeback=True)
     gC = game["GameController"]
-    return gc
+    game.close()
+    return gC
+
+def setGameController(gC):
+    cookie = SimpleCookie()
+    http_cookie_header = environ.get('HTTP_COOKIE')
+    if not http_cookie_header:
+        print("Error! Cookie header")
+        exit()
+    cookie.load(http_cookie_header)
+    if not 'playerID' in cookie:
+        print("Error! Player ID not in cookie")
+        exit()
+    playerID = cookie['playerID'].value
+    player_session = open('player_sessions/sess_' + playerID, writeback=False)
+    gameID = player_session['gameID']
+    player_session.close()
+    game = open("game_sessions/sess_" + gameID, writeback=True)
+    game["GameController"] = gC
+    game.close()
