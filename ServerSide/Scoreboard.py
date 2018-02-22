880279e67f7d116fa5bdcff14104e45fee8caf5a
StephenOR <35529975+StephenOR@users.noreply.github.com>
2018-02-22 14:39:41 +0000

    Add files via upload

---
+++ ServerSide/Scoreboard.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+
+from getGameController import *
+from ctigb import enable
+enable()
+print("Content-Type: text/plain")
+print()
+
+
+def tableCreator(GameController):
+    gamecontroller=getGameController()
+    Players=GameController._players
+    Players.sort(key=lambda x: x._score,reverse=True)
+    outstr=""
+    outstr+=("""
+        <table id ="scoreboard">
+                <tr>
+                    <th>Player</th>
+                    <th>Score</th>
+                    <th>Meeples Remaining</th>
+                </tr>\n""")
+    for i in Players:
+        outstr+=("""
+                <tr>
+                    <th>%s</th>
+                    <th>%i</th>
+                    <th>%i</th>
+                </tr>\n"""%(i._name,i._score,len(i._inactiveMeeples)))
+    outstr+=("""
+        </table>""")
+    return outstr
+
+gc=getGameController()
+table=tableCreator(gc)
+print(table)
