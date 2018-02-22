ed35b7ea42e317d44cde185bfbee1e6e58d7d194
Claire Foran <foranclaire@gmail.com>
2018-02-22 14:38:32 +0000

    Create getMeepleImage.py

---
+++ Website/getMeepleImage.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+# Return current player
+from cgitb import enable 
+enable()
+
+from getGameController import *
+print('Content-Type: text/plain')
+print()
+
+gC = getGameController()
+imageSrc = gC._playing._img
+print(imageSrc)
+setGameController(gC)
