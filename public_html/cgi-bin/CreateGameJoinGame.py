28f3fe6f71c50e33057662e5310857f301b8a5e0
Catherine Bowen <catherinebowen96@gmail.com>
2018-02-23 10:42:34 +0000

    Add files via upload

--- public_html/cgi-bin/CreateGameJoinGame.py
+++ public_html/cgi-bin/CreateGameJoinGame.py
@@ -18,7 +18,7 @@ def makePlayerSession(playerID, gameID, index):
     so that the player can get their gameID easily
     + get which position they are in gc._players (to check if it's their go) etc
     """
-    player_session = open("player_sessions/sess_" + playerID, writeback=True)
+    player_session = open("../player_sessions/sess_" + playerID, writeback=True)
     player_session["gameID"] = gameID
     player_session["index"] = index
     player_session.close()
@@ -27,22 +27,23 @@ def makePlayerSession(playerID, gameID, index):
 def newGame(playerID, player):
     """Makes a new game for the player."""
     try:
-        game_sessions = open("game_sessions/sessionlist", writeback=True)
+        game_sessions = open("../game_sessions/sessionlist", writeback=True)
         # create a new entry in game_sessions
         gameID = sha256(repr(time()).encode()).hexdigest()
         game_sessions[gameID] = [player]
+        player_list = game_sessions[gameID]
         index = 0
         game_sessions.close()
         #print(list(game_sessions.keys()))
         # create a game, create a GameController for it and add the player
-        game = open("game_sessions/sess_" + gameID, writeback=True)
+        game = open("../game_sessions/sess_" + gameID, writeback=True)
         gc = GameController()
         gc.joinGame(player)
         game["GameController"] = gc
         game.close()
         # make player session
         makePlayerSession(playerID, gameID, index)
-        return str([pl._name for pl in player_list])
+        return str([pl._id for pl in player_list])
     except IOError:
         return "IOError"
 
@@ -50,7 +51,7 @@ def newGame(playerID, player):
 def joinGame(gameID, playerID, player):
     """Adds player to an already existing game."""
     try:
-        game_sessions = open("game_sessions/sessionlist", writeback=True)
+        game_sessions = open("../game_sessions/sessionlist", writeback=True)
         # add to entry in game_sessions
         player_list = game_sessions[gameID]
         index = len(player_list)
@@ -58,40 +59,38 @@ def joinGame(gameID, playerID, player):
         game_sessions[gameID] = player_list
         game_sessions.close()
         # add player to GameController
-        game = open("game_sessions/sess_" + gameID, writeback=True)
+        game = open("../game_sessions/sess_" + gameID, writeback=True)
         gc = game["GameController"]
         gc.joinGame(player)
         game["GameController"] = gc
         game.close()
         # make player session
         makePlayerSession(playerID, gameID, index)
-        return str([pl._name for pl in player_list])
+        return str([pl._id for pl in player_list])
     except IOError:
         return "IOError"
 
 
 result = ""
 mode = ""
-name = ""
 form_data = FieldStorage()
 if len(form_data) != 0:
     # mode should be start or join
     mode = escape(form_data.getfirst("mode","").strip())
-    name = escape(form_data.getfirst("name", "").strip())
 
 # we might be able to get rid of the index variable in the player session
 # and make it easier to set player._name and player._colour at the same time
 # by changing Player's constructor to take playerID as player._id and then use
 # setters for colour and name
 playerID = sha256(repr(time()).encode()).hexdigest()
-player = Player(name)
+player = Player(playerID)
 player.createMeeples()
 
 if mode == "start":
     result = newGame(playerID, player)
 else:
     gameFound = None
-    game_sessions = open("game_sessions/sessionlist", writeback=True)
+    game_sessions = open("../game_sessions/sessionlist", writeback=True)
     for gameID in game_sessions:
         player_list = game_sessions[gameID]
         if player_list != None and len(player_list) < 4:
@@ -110,7 +109,7 @@ print(cookie)
 
 
 #values here to be changed. Not sure what to print
-print("Content-Type:text/plain")
+print("Content-Type:text/html")
 print()
 print("""
 <!DOCTYPE HTML>
