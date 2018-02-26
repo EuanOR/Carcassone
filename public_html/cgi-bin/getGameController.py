03a596a714bbe5b2fa7db9d25738c99eccfe1c6a
Eimear Foley <eimearfoley96@gmail.com>
2018-02-26 16:21:12 +0000

    Included try/except for unavailable resource
    Errno 11

--- public_html/cgi-bin/getGameController.py
+++ public_html/cgi-bin/getGameController.py
@@ -4,23 +4,30 @@ from shelve import open
 
 # Get game controller instance from session
 def getGameController():
-    cookie = SimpleCookie()
-    http_cookie_header = environ.get('HTTP_COOKIE')
-    if not http_cookie_header:
-        print("Error! Cookie header")
-        exit()
-    cookie.load(http_cookie_header)
-    if not 'playerID' in cookie:
-        print("Error! Player ID not in cookie")
-        exit()
-    playerID = cookie['playerID'].value
-    player_session = open('../player_sessions/sess_' + playerID, writeback=False)
-    gameID = player_session['gameID']
-    player_session.close()
-    game = open("../game_sessions/sess_" + gameID, writeback=True)
-    gC = game["GameController"]
-    game.close()
-    return gC
+    try:
+        cookie = SimpleCookie()
+        http_cookie_header = environ.get('HTTP_COOKIE')
+        if not http_cookie_header:
+            print("Error! Cookie header")
+            exit()
+        cookie.load(http_cookie_header)
+        if not 'playerID' in cookie:
+            print("Error! Player ID not in cookie")
+            exit()
+        playerID = cookie['playerID'].value
+        player_session = open('../player_sessions/sess_' + playerID, writeback=False)
+        gameID = player_session['gameID']
+        player_session.close()
+        game = open("../game_sessions/sess_" + gameID, writeback=False)
+        gC = game["GameController"]
+        game.close()
+        return gC
+    except Exception as e:
+        # if resource unavailable, wait and try again
+        if e.errno == 11:
+            import time
+            time.sleep(0.5)
+            return getGameController()
 
 def setGameController(gC):
     cookie = SimpleCookie()
