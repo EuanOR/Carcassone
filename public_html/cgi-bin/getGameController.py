507c0eac39d2298233fb5f27d213613ac4ef87b3
Eimear Foley <eimearfoley96@gmail.com>
2018-02-28 13:20:58 +0000

    Updated for temporarily unavailable resource error
    Included try/except for err.no == 11 for server lags

--- public_html/cgi-bin/getGameController.py
+++ public_html/cgi-bin/getGameController.py
@@ -2,7 +2,6 @@ from http.cookies import SimpleCookie
 from os import environ
 from shelve import open
 
-# Get game controller instance from session
 def getGameController():
     try:
         cookie = SimpleCookie()
@@ -30,19 +29,26 @@ def getGameController():
             return getGameController()
 
 def setGameController(gC):
-    cookie = SimpleCookie()
-    http_cookie_header = environ.get('HTTP_COOKIE')
-    if not http_cookie_header:
-        print("Error! Cookie header")
-        exit()
-    cookie.load(http_cookie_header)
-    if not 'playerID' in cookie:
-        print("Error! Player ID not in cookie")
-        exit()
-    playerID = cookie['playerID'].value
-    player_session = open('../player_sessions/sess_' + playerID, writeback=False)
-    gameID = player_session['gameID']
-    player_session.close()
-    game = open("../game_sessions/sess_" + gameID, writeback=True)
-    game["GameController"] = gC
-    game.close()
+    try:
+        cookie = SimpleCookie()
+        http_cookie_header = environ.get('HTTP_COOKIE')
+        if not http_cookie_header:
+            print("Error! Cookie header")
+            exit()
+        cookie.load(http_cookie_header)
+        if not 'playerID' in cookie:
+            print("Error! Player ID not in cookie")
+            exit()
+        playerID = cookie['playerID'].value
+        player_session = open('../player_sessions/sess_' + playerID, writeback=False)
+        gameID = player_session['gameID']
+        player_session.close()
+        game = open("../game_sessions/sess_" + gameID, writeback=True)
+        game["GameController"] = gC
+        game.close()
+    except Exception as e:
+        # if resource unavailable, wait and try again
+        if e.errno == 11:
+            import time
+            time.sleep(0.5)
+            return setGameController(gC)
