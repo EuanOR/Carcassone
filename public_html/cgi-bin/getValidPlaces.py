1b27ccde8c3ff12946c7f88b58f05c9cb7cf3b6d
Eimear Foley <eimearfoley96@gmail.com>
2018-02-28 13:15:51 +0000

    Fixed for tile rotations

--- public_html/cgi-bin/getValidPlaces.py
+++ public_html/cgi-bin/getValidPlaces.py
@@ -3,14 +3,22 @@
 # Get available and valid places according to tile rotation
 from cgitb import enable 
 enable()
-from cgi import FieldStorage, escape
+
 from getGameController import *
 print('Content-Type: text/plain')
 print()
+from cgi import FieldStorage, escape
 gC = getGameController()
+
 form_data = FieldStorage()
-rotation = form_data.getfirst('rotation').escape()
-for i in range(int(rotation)):
-    validPlaces = gC.rotateTile()
-print(validPlaces)
+if len(form_data) != 0:
+	rotate = form_data.getfirst("rotate")
+	if rotate == "True":
+		validPlaces = gC.rotateTile()
+	else:
+		validPlaces = gC.getValidTilePlacements()
 setGameController(gC)
+ret_str = ""
+for cell in validPlaces:
+	ret_str += "%s,%s " %(cell[0], cell[1])
+print(ret_str)
