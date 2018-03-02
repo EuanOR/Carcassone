f772bcc06778f5e51223744c118fc23d2ff23e5b
Eimear Foley <eimearfoley96@gmail.com>
2018-03-02 15:50:05 +0000

    Add files via upload

---
+++ public_html/cgi-bin/placeTile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from cgitb import enable 
+enable()
+
+from getGameController import *
+from cgi import FieldStorage, escape
+print('Content-Type: text/plain')
+print()
+
+gC = getGameController()
+
+form_data = FieldStorage()
+if len(form_data) != 0:
+    cell = form_data.getfirst("cellID")
+    x, y = cell.split(",")
+    gC.placeTile(int(x),int(y))
+else:
+    print("problem")
+
+setGameController(gC)
+
